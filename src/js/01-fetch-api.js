1d2293cf3d5ea84f7da87a83fc34b240c90c114b
Oleksandr <115025828+g0d2illa@users.noreply.github.com>
2023-03-27 21:43:20 +0300

    create modul 10

---
+++ src/js/01-fetch-api.js
@@ -0,0 +1,58 @@
+import '../css/common.css';
+import template from '../templates/pokemon-card.hbs';
+import API from './api-service';
+
+const pokemonContainer = document.querySelector('.js-card-container');
+const searchForm = document.querySelector('.js-search-form');
+searchForm.addEventListener('submit', onSearchForm);
+
+function onSearchForm(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const swarchQuery = form.elements.query.value;
+
+  API.fetchPokemonById(swarchQuery)
+    .then(renderPokemonCard)
+    .catch(onError)
+    .finally(form.reset());
+}
+
+function renderPokemonCard(pokemon) {
+  const markup = template(pokemon);
+  pokemonContainer.innerHTML = markup;
+}
+
+function onError(error) {
+  alert('Упс, что то пошло не так');
+}
+
+// function fetchPokemonById(pokemonId) {
+//   return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`).then(r =>
+//     r.json(),
+//   );
+// }
+
+// function renderPokemonCard(pokemon) {
+//   const markup = `<div class="card">
+//     <div class="card-img-top">
+//         <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+//     </div>
+//     <div class="card-body">
+//         <h2 class="card-title">Имя: ${pokemon.name}</h2>
+//         <p class="card-text">Вес: ${pokemon.weight}</p>
+//         <p class="card-text">Рост: ${pokemon.height}</p>
+//         <p class="card-text"><b>Умения</b></p>
+//         <ul class="list-group"></ul>
+//        ${getPokemonAbilityName(pokemon)}
+//         </ul>
+//     </div>
+// </div>`;
+//   pokemonContainer.innerHTML = markup;
+// }
+
+// function getPokemonAbilityName(pokemon) {
+//   return pokemon.abilities
+//     .map(ability => `<li class ="list-group-item">${ability.ability.name}</li>`)
+//     .join('');
+// }
